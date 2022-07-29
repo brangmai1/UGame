41912a680078712af9b7eeee5e5cc3b512d945b6
Brang <brangmai100@gmail.com>
2022-07-29 12:35:09 +0700

    Add features development branch

---
+++ UGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UGame
+//
+//  Created by Brang Mai on 7/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
