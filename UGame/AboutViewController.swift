7c6754a136c32fa8f9c6d1349be416859d985d3c
Brang <brangmai100@gmail.com>
2022-08-10 23:27:19 +0700

    Added constraints

--- UGame/AboutViewController.swift
+++ UGame/AboutViewController.swift
@@ -6,9 +6,21 @@
 //
 
 import UIKit
+import WebKit
 
 class AboutViewController: UIViewController {
-
+    
+    @IBOutlet var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let url = Bundle.main.url(forResource: "UGame", withExtension: "html") {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+    }
+    
     @IBAction func close() {
         dismiss(animated: true, completion: nil)
     }
